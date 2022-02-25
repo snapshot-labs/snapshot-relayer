1ddf24f19f03aa0abe92285dd62d00253b532ad6
Chaitanya <yourchaitu@gmail.com>
2022-02-26 00:48:27 +0530

    Fix hash with safe version 1.1.1

--- src/api.ts
+++ src/api.ts
@@ -2,16 +2,31 @@ import express from 'express';
 import { hashMessage } from '@ethersproject/hash';
 import { getAddress } from '@ethersproject/address';
 import snapshot from '@snapshot-labs/snapshot.js';
+import fetch from 'node-fetch';
 import db from './mysql';
 
 const router = express.Router();
 
-function calculateSafeMessageHash(safe, message, chainId = 1) {
+async function calculateSafeMessageHash(safe, message, chainId = 1) {
+  const domain: { verifyingContract: string; chainId?: number } = { verifyingContract: safe };
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let safeResponse: any = null;
+  try {
+    safeResponse = await fetch(`https://safe-transaction.mainnet.gnosis.io/api/v1/safes/${safe}`).then(res =>
+      res.json()
+    );
+  } catch (e) {
+    console.log(e);
+    domain.chainId = chainId;
+  }
+  if (safeResponse?.version !== '1.1.1') {
+    domain.chainId = chainId;
+  }
   const EIP712_SAFE_MESSAGE_TYPE = {
     SafeMessage: [{ type: 'bytes', name: 'message' }]
   };
   return snapshot.utils.getHash({
-    domain: { verifyingContract: safe, chainId },
+    domain,
     types: EIP712_SAFE_MESSAGE_TYPE,
     message: { message }
   });
@@ -22,7 +37,7 @@ router.post('/message', async (req, res) => {
     const msg = JSON.parse(req.body.msg);
     const hash = hashMessage(req.body.msg);
     const address = getAddress(req.body.address);
-    const safeHash = calculateSafeMessageHash(address, hash);
+    const safeHash = await calculateSafeMessageHash(address, hash);
     const params = {
       address,
       hash: safeHash,
@@ -33,6 +48,7 @@ router.post('/message', async (req, res) => {
     console.log('Received', params);
     return res.json({ id: hash });
   } catch (e) {
+    console.log(e);
     return res.status(500).json({
       error: 'unauthorized',
       error_description: e
