3f2efdc0841c68a762fcd88445d378c57f976a3e
Chaitanya <yourchaitu@gmail.com>
2022-03-03 14:03:01 +0530

    Update src/utils.ts
    Co-authored-by: Fabien <bonustrack@users.noreply.github.com>

--- src/utils.ts
+++ src/utils.ts
@@ -4,12 +4,7 @@ import { hexValue } from '@ethersproject/bytes';
 import fetch from 'node-fetch';
 
 export async function getSafeVersion(safe) {
-  const networks = await fetch(
-    'https://raw.githubusercontent.com/snapshot-labs/snapshot.js/master/src/networks.json'
-  ).then(res => res.json());
-  const rpc = networks['1'].rpc[0];
-  const connectionInfo = typeof rpc === 'object' ? { ...rpc, timeout: 25000 } : { url: rpc, timeout: 25000 };
-  const provider = new StaticJsonRpcProvider(connectionInfo);
+  const provider = new StaticJsonRpcProvider('https://rpc.ankr.com/eth');
   const storage = await provider.getStorageAt(safe, 0);
   const abi = ['function VERSION() view returns (string)'];
   const contract = new Contract(hexValue(storage), abi, provider);
