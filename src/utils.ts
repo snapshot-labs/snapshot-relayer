7d78d06c59c48f19ee47091ddf298b9b38825cf2
Chaitanya <yourchaitu@gmail.com>
2022-03-03 14:01:13 +0530

    Remove call and call contract function directly

--- src/utils.ts
+++ src/utils.ts
@@ -3,16 +3,6 @@ import { StaticJsonRpcProvider } from '@ethersproject/providers';
 import { hexValue } from '@ethersproject/bytes';
 import fetch from 'node-fetch';
 
-async function call(provider, abi, call, options = {}) {
-  const contract = new Contract(call[0], abi, provider);
-  try {
-    const params = call[2] || [];
-    return await contract[call[1]](...params, options);
-  } catch (e) {
-    return Promise.reject(e);
-  }
-}
-
 export async function getSafeVersion(safe) {
   const networks = await fetch(
     'https://raw.githubusercontent.com/snapshot-labs/snapshot.js/master/src/networks.json'
@@ -20,8 +10,8 @@ export async function getSafeVersion(safe) {
   const rpc = networks['1'].rpc[0];
   const connectionInfo = typeof rpc === 'object' ? { ...rpc, timeout: 25000 } : { url: rpc, timeout: 25000 };
   const provider = new StaticJsonRpcProvider(connectionInfo);
-  const result = await provider.getStorageAt(safe, 0);
-
-  const safeVersionABI = ['function VERSION() view returns (string)'];
-  return await call(provider, safeVersionABI, [hexValue(result), 'VERSION', []]);
+  const storage = await provider.getStorageAt(safe, 0);
+  const abi = ['function VERSION() view returns (string)'];
+  const contract = new Contract(hexValue(storage), abi, provider);
+  return await contract.VERSION([]);
 }
