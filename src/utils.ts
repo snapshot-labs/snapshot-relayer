e7109b511baadf73d0ccebe12bec8c3f7f3b93ef
Chaitanya <yourchaitu@gmail.com>
2022-06-20 13:23:27 +0530

    Use brovider

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ import { StaticJsonRpcProvider } from '@ethersproject/providers';
 import { hexValue } from '@ethersproject/bytes';
 
 export async function getSafeVersion(safe) {
-  const provider = new StaticJsonRpcProvider('https://rpc.ankr.com/eth');
+  const provider = new StaticJsonRpcProvider('https://brovider.xyz/1');
   const storage = await provider.getStorageAt(safe, 0);
   const abi = ['function VERSION() view returns (string)'];
   const contract = new Contract(hexValue(storage), abi, provider);
