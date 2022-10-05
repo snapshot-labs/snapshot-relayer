535fab6a882e3ab0b7cd2d62eb5b57c2c2d772bc
Chaitanya <yourchaitu@gmail.com>
2022-10-05 13:44:31 +0530

    Update schema.sql

--- src/schema.sql
+++ src/schema.sql
@@ -1,10 +1,11 @@
 CREATE TABLE messages (
   address VARCHAR(42) NOT NULL,
   hash VARCHAR(66) NOT NULL,
+  msg_hash VARCHAR(66) NOT NULL,
   ts BIGINT NOT NULL,
   payload JSON NOT NULL,
   network VARCHAR(24) NOT NULL,
   env VARCHAR(24) NOT NULL,
-  PRIMARY KEY (address, hash),
+  PRIMARY KEY (address, hash, msg_hash),
   INDEX ts (ts)
 );
