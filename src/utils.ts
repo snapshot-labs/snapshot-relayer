e4b1e808859e51a267643c10bcb4d23f7a8a923f
Dmytro Tolok <tolok.d@gmail.com>
2023-08-25 15:01:30 +0200

    feat(env): add brovider url to envs

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ import { hexValue } from '@ethersproject/bytes';
 import snapshot from '@snapshot-labs/snapshot.js';
 
 export async function getSafeVersion(safe, network) {
-  const provider = snapshot.utils.getProvider(network);
+  const provider = snapshot.utils.getProvider(network, { broviderUrl: process.env.BROVIDER_URL });
   const storage = await provider.getStorageAt(safe, 0);
   const abi = ['function VERSION() view returns (string)'];
   const contract = new Contract(hexValue(storage), abi, provider);
