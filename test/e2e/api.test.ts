eb2e1476950e60a7f3763d1c891b83bc22f0b9b2
Chaitanya <yourchaitu@gmail.com>
2026-03-17 18:44:46 +0530

    chore: add lint to test folder (#347)

--- test/e2e/api.test.ts
+++ test/e2e/api.test.ts
@@ -10,10 +10,13 @@ describe('POST /', () => {
       ['missing the data.msg key', { data: { foo: 'bar' } }]
     ];
 
-    it.each(inputs)('returns a 400 error when the input is %s', async (name, input) => {
-      const response = await request(HOST).post('/').send(input);
+    it.each(inputs)(
+      'returns a 400 error when the input is %s',
+      async (name, input) => {
+        const response = await request(HOST).post('/').send(input);
 
-      expect(response.statusCode).toBe(400);
-    });
+        expect(response.statusCode).toBe(400);
+      }
+    );
   });
 });
